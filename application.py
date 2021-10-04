8c4ff32066744a22a5cec152f68ddedd90261b0f
karim-coder <ab.amiri484@gmail.com>
2021-10-04 18:02:00 +0530

    Added some more changes

--- application.py
+++ application.py
@@ -2,22 +2,7 @@ from flask import Flask
 from flask import Blueprint, render_template
 
 
-# application = Flask(__name__)
-
-
-# @application.route('/')
-# def hello_world():
-#     return 'Hello there!'
 app = Flask(__name__)
-# def create_app():
-#     app.config['SECRET_KEY'] = 'my secret key'
-
-#     app.register_blueprint(views, url_profix='/')
-
-#     return app
-
-
-# views = Blueprint('views', __name__)
 
 
 @app.route('/')
@@ -25,7 +10,5 @@ def home():
     return render_template("home.html")
 
 
-# app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
